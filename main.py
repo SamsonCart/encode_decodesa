c717ece260f80964424563d279629ed4013c257a
SamsonCarter <129422671+SamsonCart@users.noreply.github.com>
2023-07-14 13:45:55 -0400

    Samson C - Upload Main file and encode function, missing decode function and function call in main function

---
+++ main.py
@@ -0,0 +1,36 @@
+def encode_password():
+    password = input("Enter an 8-digit password: ")
+    encoded_password = ""
+
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+
+    print("Your password has been encoded and stored!\n")
+
+    return encoded_password
+
+
+def decode_password(coded_password):
+    #FIXME
+    decoded_password = None
+    return decoded_password
+
+
+def main():
+
+    option = int(input("'Please enter an option: "))
+    coded_password = None
+
+    while option != 3:
+        if option == 1:
+            coded_password = encode_password()
+        elif option == 2:
+            #decode_password()
+        else:
+            print("Invalid selection, try again.\n")
+        option = int(input("Please enter an option: "))
+
+
+
+main()
